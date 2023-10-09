05a11bd5b6e05fe863bdaae40c0ba268508950e8
HabadOlad <olad.hab@gmail.com>
2023-10-09 10:30:37 +0100

    Add files via upload

--- QuotegeneratorJS.js
+++ QuotegeneratorJS.js
@@ -1,5 +1,5 @@
 var quotes = [
-    "Success is not obtained overnight. It comes in installments; you get a little bit today, a little bit tomorrow until the whole package is given out. The day you procrastinate, you lose that day/’s success.",
+    "Success is not obtained overnight. It comes in installments; you get a little bit today, a little bit tomorrow until the whole package is given out. The day you procrastinate, you lose that day\’s success.",
     "Anyone can do any amount of work, provided it isn\’t the work he is supposed to be doing at that moment",
     "Do not wait to strike till the iron is hot; but make it hot by striking. ",
     "Set a timer and compete to beat the clock to conquer procrastination. ",
