7c5f4d8fb669dd162be4898fe390476bb934c30b
Habad Olad <olad.hab@gmail.com>
2023-10-28 12:03:54 +0100

    feat:updated links and cleaned up html

---
+++ slidePuzzleGame.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @param {number} value 
+ */
+
+function evenorOdd (value) {
+    if (value%2 == 0){
+    console.log("it's even")
+  } else {
+    console.log("it's odd")
+  }
+}
+
+evenorOdd(2)
