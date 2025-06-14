d4279797e29523da17aa0a8154140536acc4b24b
Habad Olad <olad.hab@gmail.com>
2025-06-14 23:43:06 +0100

    feat: new quotes

--- QuotegeneratorJS.js
+++ QuotegeneratorJS.js
@@ -156,6 +156,8 @@ var quotes = [
     "That voice in your head isn't you. It's a voice you have heard. You have grown up with. You have learned. The best thing about learning is that it is never finished; you can learn a new voice.",
     "Nothing is quite as beautiful as someone who has survived losing everything and still has a tender heart",
     "My life is nothing but pressure. All pressure. This pressure is like a heaviness. It's always on top of me, this heaviness. It's always there since I'm a kid. Other people wake up in the morning, 'A new day! Ah, up and at 'em!' I wake up, the heaviness is waiting for me nice. Sometimes I even talk to it. I say [adopts cheerful voice] 'Hi, heaviness!' and the heaviness looks back at me, [in an ominous growl] 'Today you're gonna get it good. You'll be drinking early today.'",
+    "I, not events, have the power to make me happy or unhappy today. I can choose which it shall be. Yesterday is dead, tomorrow hasn't arrived yet. I have just one day, today, and I'm going to be happy in it.",
+    "“The only real laughter comes from despair.”",
     
     
     
