b99e94aaf245a6029c806de1f1944feb21ccbd10
Habad Olad <olad.hab@gmail.com>
2025-10-15 04:52:57 +0100

    feat:styling

--- firstWebsiteJS.js
+++ firstWebsiteJS.js
@@ -2,7 +2,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
@@ -20,31 +20,39 @@ function updateClock() {
   var seconds = now.getSeconds();
 
   // Pad single digit minutes and seconds with leading zeros
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  seconds = seconds < 10 ? '0' + seconds : seconds;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
-// Determine if it's AM or PM
-var amPM = hours >= 12 ? 'PM' : 'AM';
+  // Determine if it's AM or PM
+  var amPM = hours >= 12 ? "PM" : "AM";
 
-// Convert hours to 12-hour format
-hours = hours % 12;
-hours = hours ? hours : 12; // If hours is 0, make it 12
- 
-// Display date
-var dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-var dateString = now.toLocaleDateString(undefined, dateOptions);
+  // Convert hours to 12-hour format
+  hours = hours % 12;
+  hours = hours ? hours : 12; // If hours is 0, make it 12
 
-var timeString = `${hours}:${minutes}:${seconds}`;
-let timezoneOffset = 0
-if (now.getTimezoneOffset() !== 0) {
-  timezoneOffset = Math.abs(now.getTimezoneOffset() / 60)
-}
-let timezonePolarity = now.getTimezoneOffset() > 0 ? "-" : "+"
-const offsetString = !!timezoneOffset ? timezonePolarity + timezoneOffset : ''
-var gmtString = `GMT ${offsetString}`;
+  // Display date
+  var dateOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  var dateString = now.toLocaleDateString(undefined, dateOptions);
 
-// Display GMT
-document.getElementById('date').innerText = timeString + amPM + ' | ' + dateString + ' | ' + gmtString;
+  var timeString = `${hours}:${minutes}:${seconds}`;
+  let timezoneOffset = 0;
+  if (now.getTimezoneOffset() !== 0) {
+    timezoneOffset = Math.abs(now.getTimezoneOffset() / 60);
+  }
+  let timezonePolarity = now.getTimezoneOffset() > 0 ? "-" : "+";
+  const offsetString = !!timezoneOffset
+    ? timezonePolarity + timezoneOffset
+    : "";
+  var gmtString = `GMT ${offsetString}`;
+
+  // Display GMT
+  document.getElementById("date").innerText =
+    timeString + amPM + " | " + dateString + " | " + gmtString;
 }
 
 // Call updateClock function every second
