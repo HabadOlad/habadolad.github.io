c01af88fee78bbfe3a5a3ba4307d1173fbd282d8
Habad Olad <olad.hab@gmail.com>
2025-10-20 10:08:13 +0100

    feat:updated bio

--- assets/scripts/firstWebsiteJS.js
+++ assets/scripts/firstWebsiteJS.js
@@ -6,8 +6,17 @@ navigation.innerHTML = `  <ul>
 <li><a>BLOG</a></li>
 <li><a>CONTACT</a></li>
 <li id="date"></li>
-</ul>
-<footer>©2025 Habad Olad. All rights reserved.</footer>`;
+</ul>`;
+
+const aboutTheAuthor = document.getElementById("aboutTheAuthor");
+aboutTheAuthor.innerHTML = ` Currently living my dream job as a software engineer! Prior i was a qualified tutor and postgraduate researcher with a background in consulting. Worked on the UK’s most high-profile infrastructure developments such as High Speed 2 and Heathrow terminal expansion. Skilled in creating positive relationships with customers and stakeholders. Experienced in data analytics, tutoring, project management, database administration and consulting.
+
+My earliest programming experience was with Neopets back in the 90's/early 2000s. I initially would make mini websites for my neopets and change the colour of the font with html. For some odd reason i never pursued this further and veered away from the programming and had a passion for the environment. I always felt intimidated by programmers, thinking they were mathematical savants but it was just insecruity on my part. I studied geography as an undergrad and a masters in environmental technology. My first job was in consulting, although rewarding i knew it was not my forte. I moved onto data analytics which meant i had to learn python to manipulate CSV files and create graphs using matplotlib/numpy/seaborn (and a little bit of VB basic!). This reinvigorated my passion for code. Thankfully i found a wonderful bootcamp called Founders and Coders which aided me in my journey. This website not only showcases my skill with html,css and javascript it's also an aide with a plethora of links, motivational quotes and fun little games!
+
+Habad Olad holds a Msc in Environmental Technology from Imperial College London and a Bsc in Geography and Geology from the University of Hull.`;
+
+const footer = document.getElementById("footer");
+footer.innerHTML = `<footer>©2025 Habad Olad. All rights reserved.</footer>`;
 
 function updateClock() {
   var now = new Date();
