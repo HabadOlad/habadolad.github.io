76a246771d7377d9af5d6924a22430477cef3efb
Habad Olad <olad.hab@gmail.com>
2025-11-06 12:43:43 +0000

    feat:new stylings and menu

--- assets/scripts/firstWebsiteJS.js
+++ assets/scripts/firstWebsiteJS.js
@@ -32,6 +32,16 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+/* Toggle the "responsive" class to show/hide the navigation links */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.classList.contains("responsive")) {
+    x.classList.remove("responsive");
+  } else {
+    x.classList.add("responsive");
+  }
+}
+
 function updateClock() {
   var now = new Date();
   var hours = now.getHours();
